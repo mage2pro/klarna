f3d244ab52c6dd7b56fdec54619608309d6e17af
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-12-31 16:39:43 +0300

    no message

--- Method.php
+++ Method.php
@@ -4,14 +4,14 @@ namespace Dfe\Klarna;
 use Magento\Sales\Model\Order as O;
 use Magento\Sales\Model\Order\Address as OrderAddress;
 use Magento\Sales\Model\Order\Payment as OP;
-class Method extends \Df\Payment\R\Method {
+class Method extends \Df\PaypalClone\Method {
 	/**
 	 * 2016-12-17
 	 * Первый параметр — для test, второй — для live.
 	 * @override
-	 * @see \Df\Payment\R\Method::stageNames()
-	 * @used-by \Df\Payment\R\Method::getConfigPaymentAction()
-	 * @used-by \Df\Payment\R\Refund::stageNames()
+	 * @see \Df\PaypalClone\Method::stageNames()
+	 * @used-by \Df\PaypalClone\Method::getConfigPaymentAction()
+	 * @used-by \Df\PaypalClone\Refund::stageNames()
 	 * @return string[]
 	 */
 	public function stageNames() {return ['-stage', ''];}
@@ -19,8 +19,8 @@ class Method extends \Df\Payment\R\Method {
 	/**
 	 * 2016-12-17
 	 * @override
-	 * @see \Df\Payment\R\Method::redirectUrl()
-	 * @used-by \Df\Payment\R\Method::getConfigPaymentAction()
+	 * @see \Df\PaypalClone\Method::redirectUrl()
+	 * @used-by \Df\PaypalClone\Method::getConfigPaymentAction()
 	 * @return string
 	 */
 	protected function redirectUrl() {return '';}
