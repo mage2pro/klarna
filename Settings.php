e28efb0eeeff578c549e6d96c127370d3c08c3d3
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-01-25 10:57:21 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -1,6 +1,8 @@
 <?php
 // 2017-01-24
 namespace Dfe\Klarna;
+use Magento\Framework\App\ScopeInterface as IScope;
+use Magento\Store\Model\Store;
 /** @method static Settings s() */
 final class Settings extends \Df\Payment\Settings {
 	/**
@@ -24,25 +26,29 @@ final class Settings extends \Df\Payment\Settings {
 	;}
 
 	/**
-	 * 2016-12-24
+	 * 2017-01-24
 	 * @return string
 	 */
 	public function country() {return $this->testable();}
 
 	/**
-	 * 2016-12-24
+	 * 2017-01-24
+	 * @override
+	 * @see \Df\Payment\Settings::currency()
+	 * @used-by \Df\Payment\Settings::_cur()
+	 * @param null|string|int|IScope|Store $s [optional]
 	 * @return string
 	 */
-	public function currency() {return $this->testable();}
+	public function currency($s = null) {return $this->testable(null, $s);}
 
 	/**
-	 * 2016-12-24
+	 * 2017-01-24
 	 * @return int
 	 */
 	public function merchantID() {return $this->testable();}
 
 	/**
-	 * 2016-12-24
+	 * 2017-01-24
 	 * @return string
 	 */
 	public function sharedSecret() {return $this->testableP();}
