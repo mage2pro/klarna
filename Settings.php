38ee032cc81a544ae2fcb41411ae5c3ee05331c6
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-01-25 04:52:28 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -7,6 +7,12 @@ final class Settings extends \Df\Payment\Settings {
 	 * 2017-01-24
 	 * https://developers.klarna.com/api/?json#api-urls
 	 * https://github.com/klarna/kco_rest_php/blob/v2.2.0/src/Klarna/Rest/Transport/ConnectorInterface.php#L37-L55
+	 * 2017-01-25
+	 * @todo Как оказалось, адреса API зависят не от страны продавца, а от страны покупателя:
+	 * «Are the Klarna's API URLs based on a merchant's country or on a buyer's country?»
+	 * https://mage2.pro/t/2517
+	 * «Why are Klarna's API URLs for USA buyers different from others?» https://mage2.pro/t/2516
+	 * Поэтому текущий алгоритм неверен и нуждается в переделке.
 	 * @return string
 	 */
 	public function apiUrl() {return dfc($this, function() {return
