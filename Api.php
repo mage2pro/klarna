85c0bc4d1b4f6d861a19aa5fd2afeb1d5bce4e33
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-01-26 20:37:16 +0300

    no message

--- Api.php
+++ Api.php
@@ -101,26 +101,8 @@ final class Api {
 		/**
 		 * 2017-01-26
 		 * Checkout API версии 3.
-		 * Сюда мы попадаем в 2 случаях:
-		 *
-		 * Случай 1)
-		 * Klarna нас не авторизовала:
-		 * @see \Klarna\Rest\Transport\Connector::send():
-				if ($response->getHeader('Content-Type') !== 'application/json') {
-					throw $e;
-				}
-		 * https://github.com/klarna/kco_rest_php/blob/v2.2.0/src/Klarna/Rest/Transport/Connector.php#L132-L134
-		 * В этом случае ответ сервера имеет тип «text/html», а не «application/json».
-		 * При этом тело ответа пусто, а код HTTP ответа — 401.
-		 *
-		 * Случай 2)
-		 * Klarna вернула непонятный ответ:
-		 * @see \Klarna\Rest\Transport\Connector::send():
-				$data = $response->json();
-				if (!is_array($data) || !array_key_exists('error_code', $data)) {
-					throw $e;
-				}
-		 * https://github.com/klarna/kco_rest_php/blob/v2.2.0/src/Klarna/Rest/Transport/Connector.php#L136-L140
+		 * Сюда мы попадаем в 2 случаях, описанных здесь:
+		 * https://github.com/mage2pro/klarna/blob/0.0.5/V3/Exception/Guzzle.php?ts=4#L7-L24
 		 */
 		catch (\GuzzleHttp\Exception\ClientException $e) {
 			throw new Exception3_Guzzle($e, $request);
