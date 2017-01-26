efb8a329f2af5ea62f3cafcaa1ad3e8d06b5fab6
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-01-26 22:26:36 +0300

    no message

--- V2/Charge.php
+++ V2/Charge.php
@@ -9,7 +9,40 @@ class Charge {
 	 * @used-by p()
 	 * @return array(string => mixed)
 	 */
-	private function kl_order() {return [];}
+	private function kl_order() {return [
+		/**
+		 * 2017-01-26
+		 * «Locale indicative for language & other location-specific details (RFC1766)»
+		 * Required: yes.
+		 * Type: string.
+		 * «Which locales are supported by the version 2 of Klarna Checkout API?»
+		 * https://mage2.pro/t/2533
+		 */
+		'locale' => ''
+		/**
+		 * 2017-01-26
+		 * «Country in which the purchase is done (ISO-3166-alpha2)»
+		 * Required: yes.
+		 * Type: string.
+		 */
+		,'purchase_country' => ''
+		/**
+		 * 2017-01-26
+		 * «Currency in which the purchase is done (ISO-4217)»
+		 * Required: yes.
+		 * Type: string.
+		 */
+		,'purchase_currency' => ''
+		/**
+		 * 2017-01-26
+		 * «Only in Sweden, Norway and Finland:
+		 * Indicates whether this purchase is a recurring order»
+		 * https://developers.klarna.com/en/se/kco-v2/checkout/use-cases#Recurring-Orders
+		 * Required: no.
+		 * Type: boolean.
+		 */
+		,'recurring' => ''
+	];}
 
 	/**
 	 * 2017-01-26
