e65be7682106b8cfc40c396822335bd5ba2b828b
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-06 00:23:22 +0300

    no message

--- Method.php
+++ Method.php
@@ -4,4 +4,4 @@ namespace Dfe\Klarna;
 use Magento\Sales\Model\Order as O;
 use Magento\Sales\Model\Order\Address as OrderAddress;
 use Magento\Sales\Model\Order\Payment as OP;
-class Method extends \Df\PaypalClone\Method {}
+final class Method extends \Df\PaypalClone\Method {}
