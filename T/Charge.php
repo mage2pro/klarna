a8469f369d49fd0475c268d62661b8e5c948b309
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-11 05:04:13 +0300

    no message

--- T/Charge.php
+++ T/Charge.php
@@ -4,10 +4,7 @@ namespace Dfe\Klarna\T;
 use Dfe\Klarna\Api;
 use Dfe\Klarna\Settings as S;
 final class Charge extends \Df\Core\TestCase {
-	/**
-	 * @test
-	 * 2017-01-22
-	 */
+	/** @test 2017-01-22 */
 	public function t01() {
 		try {
 			/**
