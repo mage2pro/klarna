7bfcdbe00fb8ea45b0f8760873e8112d9cd2053a
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-03 23:28:13 +0300

    no message

---
+++ ConfigProvider.php
@@ -0,0 +1,13 @@
+<?php
+namespace Dfe\Klarna;
+// 2017-04-03
+final class ConfigProvider extends \Df\Payment\ConfigProvider {
+	/**
+	 * 2017-04-03
+	 * @override
+	 * @see \Df\Payment\ConfigProvider::config()
+	 * @used-by \Df\Payment\ConfigProvider::getConfig()
+	 * @return array(string => mixed)
+	 */
+	protected function config() {return ['html' => 'ПРЕВЕД, МЕДВЕД!'] + parent::config();}
+}
