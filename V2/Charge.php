049c7afdf429ac6c7d23b3d1e99c303e31ec2f1f
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-03 23:39:40 +0300

    no message

--- V2/Charge.php
+++ V2/Charge.php
@@ -13,7 +13,7 @@ final class Charge {
 	 * @param string $code [optional]
 	 * @return string|bool
 	 */
-	final function buyerCountry($code = null) {return
+	function buyerCountry($code = null) {return
 		!$code ? $this->_buyerCountry : $code === $this->_buyerCountry
 	;}
 
@@ -23,9 +23,9 @@ final class Charge {
 	 * @used-by kl_order()
 	 * @return string
 	 */
-	final function currency() {return dfc($this, function() {return
-		df_currency_by_country_c($this->buyerCountry())
-	;});}
+	function currency() {return dfc($this, function() {return df_currency_by_country_c(
+		$this->buyerCountry()
+	);});}
 
 	/**
 	 * 2017-02-02
@@ -34,9 +34,7 @@ final class Charge {
 	 * @used-by \Dfe\Klarna\V2\Charge\Products::p()
 	 * @return string
 	 */
-	final function locale() {return dfc($this, function() {return
-		df_locale_by_country($this->buyerCountry())
-	;});}
+	function locale() {return dfc($this, function() {return df_locale_by_country($this->buyerCountry());});}
 
 	/**
 	 * 2017-01-29
@@ -49,115 +47,93 @@ final class Charge {
 	 * 2017-01-26
 	 * https://developers.klarna.com/en/se/kco-v2/checkout-api#resource-properties
 	 * https://developers.klarna.com/en/se/kco-v2/checkout/2-embed-the-checkout#configure-checkout-order
+	 * 2017-01-30
+	 * Указание здесь поля «billing_address» приведёт к сбою:
+	 * «The field 'billing_address' is read-only».
 	 * @used-by p()
 	 * @return array(string => mixed)
 	 */
 	private function kl_order() {return [
-		/**
-		 * 2017-01-26
-		 * «Additional purchase information required for some industries.»
-		 * Required: no.
-		 * Type: attachment object.
-		 * https://developers.klarna.com/en/se/kco-v2/checkout-api#attachment-object-properties
-		 *
-		 * 2017-01-28
-		 * A list of available attachment types:
-		 * https://developers.klarna.com/en/se/kco-v2/checkout-api/attachments
-		 */
+		// 2017-01-26
+		// «Additional purchase information required for some industries.»
+		// Required: no.
+		// Type: attachment object.
+		// https://developers.klarna.com/en/se/kco-v2/checkout-api#attachment-object-properties
+		//
+		// 2017-01-28
+		// A list of available attachment types:
+		// https://developers.klarna.com/en/se/kco-v2/checkout-api/attachments
 		'attachment' => (new Charge\Attachment)->p()
-		// 2017-01-30
-		// Указание здесь поля «billing_address» приведёт к сбою:
-		// «The field 'billing_address' is read-only».
-		/**
-		 * 2017-01-26
-		 * «The cart»
-		 * Required: yes.
-		 * Type: cart object.
-		 * https://developers.klarna.com/en/se/kco-v2/checkout-api#cart-object-properties
-		 */
+		// 2017-01-26
+		// «The cart»
+		// Required: yes.
+		// Type: cart object.
+		// https://developers.klarna.com/en/se/kco-v2/checkout-api#cart-object-properties
 		,'cart' => [
-			/**
-			 * 2017-01-26
-			 * «List of cart items»
-			 * Required: yes.
-			 * Type: array of cart item objects.
-			 * https://developers.klarna.com/en/se/kco-v2/checkout-api#cart-item-object-properties
-			 *
-			 * 2017-02-02
-			 * Why does Klarna not show a cart contents to the Norway and Sweden based customers,
-			 * but shows it to the all other customers? https://mage2.pro/t/2594
-			 * https://mail.google.com/mail/u/0/#sent/159fc6464717d4ab
-			 */
+			// 2017-01-26
+			// «List of cart items»
+			// Required: yes.
+			// Type: array of cart item objects.
+			// https://developers.klarna.com/en/se/kco-v2/checkout-api#cart-item-object-properties
+			//
+			// 2017-02-02
+			// Why does Klarna not show a cart contents to the Norway and Sweden based customers,
+			// but shows it to the all other customers? https://mage2.pro/t/2594
+			// https://mail.google.com/mail/u/0/#sent/159fc6464717d4ab
 			'items' => $this->kl_order_lines()
 		]
-		/**
-		 * 2017-01-26
-		 * «Information about the liable customer of the order.»
-		 * Required: no.
-		 * Type: customer object.
-		 * https://developers.klarna.com/en/se/kco-v2/checkout-api#customer-object-properties
-		 */
+		// 2017-01-26
+		// «Information about the liable customer of the order.»
+		// Required: no.
+		// Type: customer object.
+		// https://developers.klarna.com/en/se/kco-v2/checkout-api#customer-object-properties
 		,'customer' => (new Charge\Customer($this))->p()
-		/**
-		 * 2017-01-27
-		 * «External checkout providers.»
-		 * Required: no.
-		 * Type: array of external checkout objects.
-		 * https://developers.klarna.com/en/se/kco-v2/checkout-api#external_checkout-object-properties
-		 */
+		// 2017-01-27
+		// «External checkout providers.»
+		// Required: no.
+		// Type: array of external checkout objects.
+		// https://developers.klarna.com/en/se/kco-v2/checkout-api#external_checkout-object-properties
 		,'external_checkouts' => []
-		/**
-		 * 2017-01-27
-		 * «External payment methods.»
-		 * Required: no.
-		 * Type: array of external payment method objects.
-		 * https://developers.klarna.com/en/se/kco-v2/checkout-api#external_payment_method-object-properties
-		 */
+		// 2017-01-27
+		// «External payment methods.»
+		// Required: no.
+		// Type: array of external payment method objects.
+		// https://developers.klarna.com/en/se/kco-v2/checkout-api#external_payment_method-object-properties
 		,'external_payment_methods' => []
-		/**
-		 * 2017-01-26
-		 * «The gui object»
-		 * Required: no.
-		 * Type: gui object.
-		 * https://developers.klarna.com/en/se/kco-v2/checkout-api#gui-object-properties
-		 */
+		// 2017-01-26
+		// «The gui object»
+		// Required: no.
+		// Type: gui object.
+		// https://developers.klarna.com/en/se/kco-v2/checkout-api#gui-object-properties
 		,'gui' => [
-			/**
-			 * 2017-01-26
-			 * «Layout. `desktop` by default, alternatively `mobile`»
-			 * Required: no.
-			 * Type: string.
-			 */
+			// 2017-01-26
+			// «Layout. `desktop` by default, alternatively `mobile`»
+			// Required: no.
+			// Type: string.
 			'layout' => 'desktop'
-			/**
-			 * 2017-01-26
-			 * «An array of options to define the checkout behaviour.
-			 * Supported options `disable_autofocus`.»
-			 * Required: no.
-			 * Type: array of strings.
-			 */
+			// 2017-01-26
+			// «An array of options to define the checkout behaviour.
+			// Supported options `disable_autofocus`.»
+			// Required: no.
+			// Type: array of strings.
 			,'options' => []
 		]
-		/**
-		 * 2017-01-26
-		 * «Locale indicative for language & other location-specific details (RFC1766)»
-		 * Required: yes.
-		 * Type: string.
-		 * «Which locales are supported by the version 2 of Klarna Checkout API?»
-		 * https://mage2.pro/t/2533
-		 * https://developers.klarna.com/en/se/kco-v2/checkout-api#supported-locales
-		 *
-		 * 2017-01-28
-		 * Пустое значение приводит к сбою «Bad format».
-		 */
+		// 2017-01-26
+		// «Locale indicative for language & other location-specific details (RFC1766)»
+		// Required: yes.
+		// Type: string.
+		// «Which locales are supported by the version 2 of Klarna Checkout API?»
+		// https://mage2.pro/t/2533
+		// https://developers.klarna.com/en/se/kco-v2/checkout-api#supported-locales
+		//
+		// 2017-01-28
+		// Пустое значение приводит к сбою «Bad format».
 		,'locale' => $this->localeFormatted()
-		/**
-		 * 2017-01-26
-		 * «Merchant related information.»
-		 * Required: yes.
-		 * Type: merchant object.
-		 * https://developers.klarna.com/en/se/kco-v2/checkout-api#merchant-object-properties
-		 */
+		// 2017-01-26
+		// «Merchant related information.»
+		// Required: yes.
+		// Type: merchant object.
+		// https://developers.klarna.com/en/se/kco-v2/checkout-api#merchant-object-properties
 		,'merchant' => (new Charge\Merchant)->p()
 		/**
 		 * 2017-01-26
@@ -173,19 +149,15 @@ final class Charge {
 		 * https://mail.google.com/mail/u/0/#sent/159e514b238a039c
 		 */
 		,'merchant_reference' => [
-			/**
-			 * 2017-01-26
-			 * «Used for storing merchant's internal order number or other reference.»
-			 * Required: no.
-			 * Type: string.
-			 */
+			// 2017-01-26
+			// «Used for storing merchant's internal order number or other reference.»
+			// Required: no.
+			// Type: string.
 			'orderid1' => '1'
-			/**
-			 * 2017-01-26
-			 * «Used for storing merchant's internal order number or other reference.»
-			 * Required: no.
-			 * Type: string.
-			 */
+			// 2017-01-26
+			// «Used for storing merchant's internal order number or other reference.»
+			// Required: no.
+			// Type: string.
 			,'orderid2' => '1'
 		]
 		/**
