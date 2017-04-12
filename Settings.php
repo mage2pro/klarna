e54a6d79ccff5e6ef22435f1f28c1a24f9aefa79
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-12 12:16:09 +0300

    0.2.9

--- Settings.php
+++ Settings.php
@@ -6,13 +6,6 @@ final class Settings extends \Df\Payment\Settings {
 	/**
 	 * 2017-01-24
 	 * @used-by \Dfe\Klarna\Api\Checkout::html()
-	 * @return int
-	 */
-	function merchantID() {return $this->testable();}
-
-	/**
-	 * 2017-01-24
-	 * @used-by \Dfe\Klarna\Api\Checkout::html()
 	 * @return string
 	 */
 	function sharedSecret() {return $this->testableP();}
