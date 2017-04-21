e8a2b7b300fce0df2b08a841e466000857f2ae52
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-21 08:04:14 +0300

    no message

--- Button.php
+++ Button.php
@@ -1,7 +1,11 @@
 <?php
 namespace Dfe\Klarna;
 use Magento\Framework\View\Element\AbstractBlock as _P;
-// 2017-04-21
+/**
+ * 2017-04-21
+ * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
+ * @used-by https://github.com/mage2pro/klarna/blob/0.3.1/view/frontend/layout/default.xml#L6
+ */
 class Button extends _P {
 	/**
 	 * 2017-04-21
