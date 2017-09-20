0f5d8f3416f9f5b4bc5caab207cacf6f9f49f4e3
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-20 12:43:58 +0300

    0.4.1

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -4,7 +4,7 @@ define([
 	,'Magento_Checkout/js/model/quote'
 	,'Magento_Customer/js/model/customer'
 	,'Magento_Checkout/js/model/url-builder'
-], function(df, api, billingAddressChange, parent, $, ko, q, customer, ub) {'use strict';
+], function(df, api, baChange, parent, $, ko, q, customer, ub) {'use strict';
 /** 2017-09-06 @uses Class::extend() https://github.com/magento/magento2/blob/2.2.0-rc2.3/app/code/Magento/Ui/view/base/web/js/lib/core/class.js#L106-L140 */
 return parent.extend({
 	// 2016-12-17
@@ -21,8 +21,7 @@ return parent.extend({
 	initialize: function() {
 		this._super();
 		this.klHtml = ko.observable(this.config('html'));
-		var _this = this;
-		billingAddressChange(function(newAddress) {if (newAddress.countryId) {
+		baChange(this, function(newAddress) {if (newAddress.countryId) {
 			// 2017-04-04
 			// The M2 client part does not notify the server part about the billing address change.
 			// So we need to pass the chosen country ID to the server part.
@@ -30,21 +29,21 @@ return parent.extend({
 			//_this.klHtml(newAddress.countryId);
 			/** @type {Boolean} */
 			var l = customer.isLoggedIn();
-			$.when(api(_this,
+			$.when(api(this,
 				// 2017-04-05
 				// Для анонимных покупателей q.getQuoteId() — это строка вида
 				// «63b25f081bfb8e4594725d8a58b012f7».
 				ub.createUrl(df.s.t('/dfe-klarna/%s/html', l ? 'mine' : q.getQuoteId()), {})
-				,df.o.merge({ba: q.billingAddress(), qp: _this.getData()}, l ? {} : {email: q.guestEmail})
+				,df.o.merge({ba: q.billingAddress(), qp: this.getData()}, l ? {} : {email: q.guestEmail})
 			))
 				.fail(function() {debugger;})
-				.done(function(json) {
+				.done($.proxy(function(json) {
 					// 2017-04-05
 					// Отныне json у нас всегда строка: @see dfw_encode().
 					/** @type {Object} */
 					var d = !json ? {} : $.parseJSON(json);
-					_this.klHtml(d['html']);
-				})
+					this.klHtml(d['html']);
+				}, this))
 			;
 		}}, false);
 		return this;
