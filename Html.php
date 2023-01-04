8e864c218f7c1b327f50fc1895cbe9b66621a7de
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-04 21:51:59 +0000

    https://github.com/mage2pro/core/issues/174

--- Html.php
+++ Html.php
@@ -27,13 +27,12 @@ final class Html {
 	 * $qp в поле @see \Magento\Framework\DataObject::_data содержит код способа оплаты,
 	 * а также ту дополнительную информацию, которую передала клиентская часть модуля оплаты.
 	 * Например: [additional_data => [], method => "dfe_klarna"].
-	 * @param int $cartId
 	 * @param IQP|QP $qp
 	 * @param IQA|QA|null $ba
 	 * 2017-04-05 Важно возвращать именно string: @see dfw_encode()
 	 * @return string
 	 */
-	function registered($cartId, IQP $qp, IQA $ba = null) {return dfw_encode([
+	function registered(int $cartId, IQP $qp, IQA $ba = null) {return dfw_encode([
 		'html' => API::html(null, 'SE')
 	]);}
 }
