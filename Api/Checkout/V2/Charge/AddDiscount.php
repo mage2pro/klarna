52b1f2f345a841a252773407284527a44a55e86c
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-04 22:07:02 +0000

    https://github.com/mage2pro/core/issues/168

--- Api/Checkout/V2/Charge/AddDiscount.php
+++ Api/Checkout/V2/Charge/AddDiscount.php
@@ -8,7 +8,7 @@ final class AddDiscount extends Part {
 	 * @param array(array(string => string|int)) $items
 	 * @return array(array(string => string|int))
 	 */
-	function p(array $items) {
+	function p(array $items):array {
 		/** @var int $total */
 		/**
 		 * 2017-02-03
